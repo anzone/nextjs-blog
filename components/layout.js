095a327e10a2d8e1ff8c20296bddc2fc4c9c8b39
Antonio Eugenio <antonio@sinfe.net>
2023-01-10 19:18:25 +0100

    feature-3
    feature-3-description

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'feature-2.1'
+const name = 'feature-3'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
